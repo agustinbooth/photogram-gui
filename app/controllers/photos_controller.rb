6a0de4b20f2d6fa22dad8a16d1909f67750033b8
agustinvd10 <avalenzu@chicagobooth.edu>
2021-02-17 23:50:54 +0000

    delete is working

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -10,4 +10,30 @@ class PhotosController < ApplicationController
 
   end
 
+  def show
+
+  # Parameters: {"path_id"=>"777"}
+
+  url_id = params.fetch("path_id")
+  matching_photos = Photo.where({ :id => url_id})
+  @the_photo = matching_photos.at(0)
+
+   render({:template => "photo_templates/show.html.erb"})
+  end
+
+  def baii
+    
+    the_id = params.fetch("toast_id")
+
+    matching_photos = Photo.where({:id => the_id})
+
+    the_photo = matching_photos.at(0)
+
+    the_photo.destroy
+    
+    # render({:template => "photo_templates/baii.html.erb"})
+    
+    redirect_to ("/photos")
+  end
+
 end
