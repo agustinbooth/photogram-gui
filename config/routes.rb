23218c2bf68ffe53dfe67d4935c7593440215af3
agustinvd10 <avalenzu@chicagobooth.edu>
2021-02-18 15:51:59 +0000

    Photo create ready

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get("/photos/:path_id", { :controller => "photos", :action => "show"})
 
   get("/delete_photo/:toast_id", { :controller => "photos", :action => "baii"})
+  get("insert_photo", { :controller => "photos", :action => "new"})
 end
